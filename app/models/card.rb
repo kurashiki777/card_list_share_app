6f1bce3a7ce306f09b2a6399751ece30fedf3f70
kurashiki777 <shimoyuuki@icloud.com>
2024-02-28 20:58:00 +0900

    ０が自動入力

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,18 @@
 class Card < ApplicationRecord
     mount_uploader :card_image, CardImageUploader
+
     belongs_to :user
+
     has_many :comments, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 255 }
     validates :remarks, presence: true, length: { maximum: 65_535 }
+
+    before_validation :set_default_stock_quantity
+
+  private
+
+  def set_default_stock_quantity
+    self.stock_quantity ||= 0 if stock_quantity.nil?
+  end
 end
