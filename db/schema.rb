5505b44274e76000b4ca0c630d411ecd61c911e1
kurashiki777 <shimoyuuki@icloud.com>
2024-03-04 20:49:46 +0900

    パスワードリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_12_161432) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_03_142451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,8 +82,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_12_161432) do
     t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.string "avatar"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cards", "users"
